e90c962473cc356f454ff8bcbdfbad102b6de1ce
Boris Mateev <mateev.br@gmail.com>
2020-05-19 22:19:35 -0400

    feat(large-tile): added main styles

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import ReactDOM from "react-dom";
 import { App } from "./app.jsx";
+import './main.css';
 
 ReactDOM.render(
   <App />,
--- src/large-tile.jsx
+++ src/large-tile.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import largeTileCss from './large-tile.css';
 
 
 // props: {
@@ -12,21 +13,25 @@ export default function largeTile(props) {
 
 
   return (
-    <div className="large-tile">
+    <div className={`large-tile ${props.site}-border`}>
       <div className="icon-and-handle-container">
-        {props.name}
+        <img src={`../social-media-dashboard/static/images/icon-${props.site}.svg`} />
+        <span className="handle-text">{props.name}</span>
       </div>
       <div className="num-followers-container">
         <div className="followers-number">
           {props.total}
         </div>
-        <div className="followers-text">FOLLOWERS</div>
+        <div className="followers-static-text">FOLLOWERS</div>
       </div>
-      <div className={'change-in-followers-container' + props.change < 0 ? 'red' : 'green'}>
-        {props.change < 0 ? '&#9660' : '&#9650'} {props.change} Today
+      <div className={'change-in-followers-container ' + (props.change < 0 ? 'red' : 'green')}>
+        {props.change >= 0 ? '/\\' : '\\/'} {props.change} Today
       </div>
     </div>
   )
 
+  function getIconPath() {
+
+  }
 }
 
