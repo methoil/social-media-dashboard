91e48edc1a9a83c323afce0ce3ff8554db7a6fa3
Boris Mateev <mateev.br@gmail.com>
2020-05-24 19:15:47 -0400

    feat - desktop mode working properly
    - remaingin issues:
      - use actualy symbols for up/down
      - right section of tiles in desktop mode does not line up with the toggle button for larger screens
      - no gradient for instagram tile
        - need to put gradient on border

--- src/app.jsx
+++ src/app.jsx
@@ -12,27 +12,38 @@ export default function SocialMediaDashboard() {
 
   return (
     <div className={`main-container ${mode}-mode`}>
-      <div className={`title ${mode}-mode-text-2`}>Social Media Dashboard</div>
-      <div className={`${mode}-mode-text`}>Total Followers: 23,004</div>
-      <div className="horizontal-line"></div>
-
-      <div className="toggle-mode-container">
-        <span className={`mode-label ${mode}-mode-text`}>{mode} Mode</span>
-        <ToggleMode mode={mode} toggleMode={toggleMode} ></ToggleMode>
+      <div className={`top-components-container`}>
+        <div>
+          <div className={`title ${mode}-mode-text-2`}>Social Media Dashboard</div>
+          <div className={`${mode}-mode-text`}>Total Followers: 23,004</div>
+        </div>
+        <div className="horizontal-line"></div>
+        <div className="toggle-mode-container">
+          <span className={`mode-label ${mode}-mode-text`}>{mode} Mode</span>
+          <ToggleMode mode={mode} toggleMode={toggleMode} ></ToggleMode>
+        </div>
       </div>
 
-      {data.largeTiles.map(tile => {
-        return <LargeTile name={tile.name} site={tile.site} total={tile.total} change={tile.change} mode={mode}></LargeTile>
-      })}
+      <div className={`tile-container`}>
+        {data.largeTiles.map(tile => {
+          return <LargeTile name={tile.name} site={tile.site} total={tile.total} change={tile.change} mode={mode}></LargeTile>
+        })}
+      </div>
+
 
       <div className={`title ${mode}-mode-text-2`}>Overview - Today</div>
+      <div className={`tile-container`}>
+        {data.smallTiles.map((tile) => {
+          return (<SmallTile site={tile.site} meta={tile.actions} mode={mode} ></SmallTile>)
+        })}
+      </div>
+      <div className={`tile-container`}>
+        {data.smallTiles.map((tile) => {
+          return (<SmallTile site={tile.site} meta={tile.actions} mode={mode} ></SmallTile>)
+        })}
+      </div>
 
-      {data.smallTiles.map((tile) => {
-        return (<span>
-          <SmallTile site={tile.site} meta={tile.actions} mode={mode} ></SmallTile>
-          <SmallTile site={tile.site} meta={tile.likes} mode={mode} ></SmallTile>
-        </span>)
-      })}
+
     </div>
   );
 }
